b0891dcd591365684cca8140312bf5212eebf2cd
Serhii Cherneha <chernegasergiy3@gmail.com>
2025-10-26 12:15:08 +0200

    test: Overhaul test suite for OIDC validation

--- tests/Provider/XAuthConnectTest.php
+++ tests/Provider/XAuthConnectTest.php
@@ -2,8 +2,8 @@
 
 namespace ChernegaSergiy\XAuthConnect\OAuth2\Client\Tests\Provider;
 
-use ChernegaSergiy\XAuthConnect\OAuth2\Client\Exception\HostedDomainException;
 use ChernegaSergiy\XAuthConnect\OAuth2\Client\Provider\XAuthConnect;
+use Firebase\JWT\JWT;
 use GuzzleHttp\Client as HttpClient;
 use GuzzleHttp\Handler\MockHandler;
 use GuzzleHttp\Psr7\Response;
@@ -14,19 +14,21 @@ use Mockery as m;
 
 class XAuthConnectTest extends TestCase
 {
-    protected $provider;
+    protected XAuthConnect $provider;
 
     protected function setUp(): void
     {
         $this->provider = new XAuthConnect([
-            'clientId'     => 'test_client_123',
-            'clientSecret' => 'test_secret_key',
-            'redirectUri'  => 'http://127.0.0.1:8081/client.php',
+            'clientId'                => 'test_client_123',
+            'clientSecret'            => 'test_secret_key',
+            'redirectUri'             => 'http://127.0.0.1:8081/client.php',
             'baseAuthorizationUrl'    => 'http://127.0.0.1:8010/xauth/authorize',
             'baseAccessTokenUrl'      => 'http://127.0.0.1:8010/xauth/token',
             'resourceOwnerDetailsUrl' => 'http://127.0.0.1:8010/xauth/user',
             'introspectUrl'           => 'http://127.0.0.1:8010/xauth/introspect',
             'revokeUrl'               => 'http://127.0.0.1:8010/xauth/revoke',
+            'jwksUrl'                 => 'http://127.0.0.1:8010/xauth/jwks',
+            'issuer'                  => 'http://127.0.0.1:8010',
         ]);
     }
 
@@ -34,12 +36,6 @@ class XAuthConnectTest extends TestCase
     {
         m::close();
         parent::tearDown();
-    }
-
-    public function testRequiredOptions()
-    {
-        $this->expectException(\InvalidArgumentException::class);
-        new XAuthConnect([]);
     }
 
     public function testAuthorizationUrl()
@@ -74,7 +70,7 @@ class XAuthConnectTest extends TestCase
         $reflection = new \ReflectionClass($this->provider);
         $method = $reflection->getMethod('getDefaultScopes');
         $method->setAccessible(true);
-        $this->assertEquals(['profile:nickname', 'profile:uuid'], $method->invoke($this->provider));
+        $this->assertEquals(['openid', 'profile:nickname', 'profile:uuid'], $method->invoke($this->provider));
     }
 
     public function testCheckResponseThrowsException()
@@ -95,8 +91,8 @@ class XAuthConnectTest extends TestCase
     {
         $mock = new MockHandler([
             new Response(200, ['Content-Type' => 'application/json'], json_encode([
-                'profile:uuid' => '12345',
-                'profile:nickname' => 'test_user'
+                'sub' => '12345',
+                'nickname' => 'test_user'
             ]))
         ]);
 
@@ -109,46 +105,9 @@ class XAuthConnectTest extends TestCase
         $this->assertEquals('12345', $user->getId());
         $this->assertEquals('test_user', $user->getNickname());
         $this->assertEquals([
-            'profile:uuid' => '12345',
-            'profile:nickname' => 'test_user'
+            'sub' => '12345',
+            'nickname' => 'test_user'
         ], $user->toArray());
-    }
-
-    public function testIntrospectToken()
-    {
-        $mock = new MockHandler([
-            new Response(200, ['Content-Type' => 'application/json'], json_encode(['active' => true])),
-        ]);
-
-        $client = new HttpClient(['handler' => $mock]);
-        $this->provider->setHttpClient($client);
-
-        $result = $this->provider->introspectToken('test_token');
-
-        $this->assertIsArray($result);
-        $this->assertTrue($result['active']);
-    }
-
-    public function testRevokeToken()
-    {
-        $mock = new MockHandler([
-            new Response(200),
-        ]);
-
-        $client = new HttpClient(['handler' => $mock]);
-        $this->provider->setHttpClient($client);
-
-        $this->provider->revokeToken('test_token');
-        $this->assertTrue(true); // No exception thrown
-    }
-
-    public function testHostedDomainExceptionNotMatchingDomain()
-    {
-        $configuredDomain = 'example.com';
-        $exception = HostedDomainException::notMatchingDomain($configuredDomain);
-
-        $this->assertInstanceOf(HostedDomainException::class, $exception);
-        $this->assertEquals("User is not part of domain '$configuredDomain'", $exception->getMessage());
     }
 
     public function testDiscoverySuccess()
@@ -159,6 +118,7 @@ class XAuthConnectTest extends TestCase
             'userinfo_endpoint' => 'http://discovered.com/user',
             'introspection_endpoint' => 'http://discovered.com/introspect',
             'revocation_endpoint' => 'http://discovered.com/revoke',
+            'jwks_uri' => 'http://discovered.com/jwks',
         ];
 
         $mock = new MockHandler([
@@ -179,96 +139,71 @@ class XAuthConnectTest extends TestCase
         $this->assertEquals('http://discovered.com/user', $provider->getResourceOwnerDetailsUrl(new AccessToken(['access_token' => 'test'])));
     }
 
-    public function testDiscoveryManualOverride()
+    /**
+     * @dataProvider idTokenValidationProvider
+     */
+    public function testIdTokenValidation(array $payload, ?string $nonce, ?string $exception, ?string $exceptionMessage)
     {
-        $discoveryDoc = [
-            'authorization_endpoint' => 'http://discovered.com/auth',
-            'token_endpoint' => 'http://discovered.com/token',
-            'userinfo_endpoint' => 'http://discovered.com/user',
-            'introspection_endpoint' => 'http://discovered.com/introspect',
-            'revocation_endpoint' => 'http://discovered.com/revoke',
+        if ($exception) {
+            $this->expectException($exception);
+            $this->expectExceptionMessage($exceptionMessage);
+        }
+
+        $privateKey = openssl_pkey_new([
+            'private_key_bits' => 2048,
+            'private_key_type' => OPENSSL_KEYTYPE_RSA,
+        ]);
+        $publicKeyDetails = openssl_pkey_get_details($privateKey);
+
+        $kid = 'test-key-id';
+        $idToken = JWT::encode($payload, $privateKey, 'RS256', $kid);
+
+        $jwks = [
+            'keys' => [
+                [
+                    'kty' => 'RSA',
+                    'alg' => 'RS256',
+                    'kid' => $kid,
+                    'n' => rtrim(strtr(base64_encode($publicKeyDetails['rsa']['n']), '+/', '-_'), '='),
+                    'e' => rtrim(strtr(base64_encode($publicKeyDetails['rsa']['e']), '+/', '-_'), '='),
+                ]
+            ]
         ];
 
         $mock = new MockHandler([
-            new Response(200, ['Content-Type' => 'application/json'], json_encode($discoveryDoc)),
+            new Response(200, ['Content-Type' => 'application/json'], json_encode(['access_token' => 'mock_access_token', 'id_token' => $idToken])),
+            new Response(200, ['Content-Type' => 'application/json'], json_encode($jwks)),
         ]);
 
         $client = new HttpClient(['handler' => $mock]);
+        $this->provider->setHttpClient($client);
 
-        $provider = new XAuthConnect([
-            'issuer'       => 'http://discovered.com',
-            'clientId'     => 'test_client_123',
-            'clientSecret' => 'test_secret_key',
-            'redirectUri'  => 'http://127.0.0.1:8081/client.php',
-            'baseAuthorizationUrl' => 'http://manual-override.com/auth',
-        ], ['httpClient' => $client]);
+        if ($nonce) {
+            $_SESSION['oauth2nonce'] = $nonce;
+        }
 
-        $this->assertEquals('http://manual-override.com/auth', $provider->getBaseAuthorizationUrl([]));
-        $this->assertEquals('http://discovered.com/token', $provider->getBaseAccessTokenUrl([])); // This one should still be from discovery
+        $token = $this->provider->getAccessToken('authorization_code', ['code' => 'mock_code']);
+
+        $this->assertNotNull($token->getValues()['id_token_claims']);
     }
 
-    public function testDiscoveryHttpFailure()
+    public function idTokenValidationProvider(): array
     {
-        $this->expectException(\RuntimeException::class);
-        $this->expectExceptionMessage('Failed to discover endpoints');
-
-        $mock = new MockHandler([
-            new Response(500),
-        ]);
-
-        $client = new HttpClient(['handler' => $mock]);
-
-        new XAuthConnect([
-            'issuer'       => 'http://failed-discovery.com',
-            'clientId'     => 'test_client_123',
-            'clientSecret' => 'test_secret_key',
-            'redirectUri'  => 'http://127.0.0.1:8081/client.php',
-        ], ['httpClient' => $client]);
-    }
-
-    public function testDiscoveryInvalidJson()
-    {
-        $this->expectException(\RuntimeException::class);
-        $this->expectExceptionMessage('Failed to parse discovery document');
-
-        $mock = new MockHandler([
-            new Response(200, [], 'this is not json'),
-        ]);
-
-        $client = new HttpClient(['handler' => $mock]);
-
-        new XAuthConnect([
-            'issuer'       => 'http://invalid-json.com',
-            'clientId'     => 'test_client_123',
-            'clientSecret' => 'test_secret_key',
-            'redirectUri'  => 'http://127.0.0.1:8081/client.php',
-        ], ['httpClient' => $client]);
-    }
-
-    public function testDiscoveryMissingRequiredUrl()
-    {
-        $this->expectException(\InvalidArgumentException::class);
-        $this->expectExceptionMessage("The 'baseAuthorizationUrl' option is required or must be discoverable from the 'issuer' URL.");
-
-        $discoveryDoc = [
-            // authorization_endpoint is missing
-            'token_endpoint' => 'http://discovered.com/token',
-            'userinfo_endpoint' => 'http://discovered.com/user',
-            'introspection_endpoint' => 'http://discovered.com/introspect',
-            'revocation_endpoint' => 'http://discovered.com/revoke',
+        $basePayload = [
+            'iss' => 'http://127.0.0.1:8010',
+            'aud' => 'test_client_123',
+            'exp' => time() + 3600,
+            'iat' => time(),
+            'sub' => 'test-sub',
+            'nonce' => 'test-nonce'
         ];
 
-        $mock = new MockHandler([
-            new Response(200, ['Content-Type' => 'application/json'], json_encode($discoveryDoc)),
-        ]);
-
-        $client = new HttpClient(['handler' => $mock]);
-
-        new XAuthConnect([
-            'issuer'       => 'http://missing-url.com',
-            'clientId'     => 'test_client_123',
-            'clientSecret' => 'test_secret_key',
-            'redirectUri'  => 'http://127.0.0.1:8081/client.php',
-        ], ['httpClient' => $client]);
+        return [
+            'valid token' => [$basePayload, 'test-nonce', null, null],
+            'invalid issuer' => [array_merge($basePayload, ['iss' => 'http://invalid-issuer']), 'test-nonce', IdentityProviderException::class, 'Invalid issuer claim'],
+            'invalid audience' => [array_merge($basePayload, ['aud' => 'invalid-aud']), 'test-nonce', IdentityProviderException::class, 'Invalid audience claim'],
+            'missing nonce in token' => [array_merge($basePayload, ['nonce' => null]), 'test-nonce', IdentityProviderException::class, 'ID token is missing nonce claim'],
+            'invalid nonce' => [array_merge($basePayload, ['nonce' => 'invalid-nonce']), 'test-nonce', IdentityProviderException::class, 'Invalid nonce'],
+        ];
     }
 }
